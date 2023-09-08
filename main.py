bbc24ced871270ef718acc9aed0870b7bd557861
Ernestek <iernestek@gmail.com>
2023-09-08 10:19:37 +0300

    stop word

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from langdetect import detect
 
 # Файл с ключами, один ключ - одна строка
-seo = 'seo.txt'
+seo = 'seo_en.txt'
 
 
 # Чтение ключевых слов из файла
@@ -16,67 +16,67 @@ def read_keywords(file_path):
     return keywords
 
 
-# Очистка текста от стоп-слов
-def remove_stopwords(text):
-    doc = nlp(text)
-    filtered_words = [token.text for token in doc if not token.is_stop]
-    return ' '.join(filtered_words)
-
-
 # Кластеризация семантического ядра
-def cluster_keywords(keywords, num_clusters):
-    words = []
+def cluster_keywords(keywords, num_clusters, stop_words: list):
     result_keys = []
+    other = []
     for keyword in keywords:
+        stop_words = list(map(lambda x: x.lower(), stop_words))
         doc = nlp(keyword.lower())
-        result_key = " ".join([token.lemma_ for token in doc if token and not token.is_stop])
-        result_keys.append(result_key)
+        result_key = []
+        stop = False
+        for token in doc:
+            if token.text.lower() in stop_words:
+                stop = True
+                other.append(keyword)
+                break
+            else:
+                result_key.append(token.lemma_)
 
-    cleaned_keywords = list([remove_stopwords(keyword) for keyword in result_keys])
-    for i in cleaned_keywords:
-        words.extend(i.split())
+        if not stop:
+            result_key = ' '.join([token.lemma_ for token in doc if token and not token.is_stop])
+            result_keys.append(result_key)
 
     vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(cleaned_keywords)
-    # print(X)
+    X = vectorizer.fit_transform(result_keys)
     kmeans = KMeans(n_clusters=num_clusters)
     kmeans.fit(X)
+    return kmeans.labels_, result_keys, other
 
-    return kmeans.labels_
-
-
-# def write_in_xl():
 
 def main(file_path):
     # Укажите путь к файлу с ключевыми словами
+    stop_words = ['Buy']
     keywords = read_keywords(file_path)
     num_clusters = round(len(keywords)/10)  # Укажите количество желаемых кластеров
-    labels = cluster_keywords(keywords, num_clusters)
+    result_info = cluster_keywords(keywords, num_clusters, stop_words=stop_words)
 
     clusters = defaultdict(list)
-    for keyword, label in zip(keywords, labels):
+    for keyword, label in zip(result_info[1], result_info[0]):
         clusters[label].append(keyword)
-    print(len(clusters))
+
     workbook = openpyxl.Workbook()
     sheet = workbook.active
     sheet[f'A{1}'] = 'Cluster ID'
     sheet[f'B{1}'] = 'Group'
     sheet[f'C{1}'] = 'Key'
-    # for index, value in enumerate(column1_data, start=1):
-    #     sheet.cell(row=index, column=1, value=value)
 
     index = 2
     for label, cluster_keywords_list in clusters.items():
         result = f"Cluster {label + 1}: {', '.join(cluster_keywords_list)}\n"
         print(result)
-        # with open('result.txt', 'a') as file:
-        #     file.write(result + '\n')
+
         for key in cluster_keywords_list:
             sheet[f'A{index}'] = label + 1
             sheet[f'B{index}'] = cluster_keywords_list[0]
             sheet[f'C{index}'] = key
             index += 1
 
+    for key in result_info[2]:
+        sheet[f'A{index}'] = 0
+        sheet[f'B{index}'] = 'Stop_world'
+        sheet[f'C{index}'] = key
+        index += 1
     # Сохраняем файл
     workbook.save('example.xlsx')
     workbook.close()
@@ -85,7 +85,7 @@ def main(file_path):
 if __name__ == "__main__":
     text = ' '.join(read_keywords(seo))
     lang = detect(text)
-    # full_lang = to_name(lang).lower()
+
     if lang == 'en':
         spacy_lang_model = f'{lang.lower()}_core_web_sm'  # en
     else:
